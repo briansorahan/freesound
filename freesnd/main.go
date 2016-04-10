8cb1a69740ad11aaf790cf3931071397b35c1824
Brian Sorahan <bsorahan@leaf.ag>
2016-04-10 09:10:51 -0500

    fix ineffassign

--- freesnd/main.go
+++ freesnd/main.go
@@ -9,8 +9,8 @@ import (
 
 func main() {
 	key, secret := os.Getenv("FREESOUND_API_KEY"), os.Getenv("FREESOUND_API_SECRET")
-	c, err := freesound.New(key, secret)
-	if err != nil {
+	// TODO: use client
+	if _, err := freesound.New(key, secret); err != nil {
 		log.Fatal(err)
 	}
 }
