24ea9cd3ab92c6f4a2b642cd07b507af40a6cb14
Brian Sorahan <bsorahan@leaf.ag>
2016-09-22 20:12:27 -0500

    started sound instance

--- sound.go
+++ sound.go
@@ -10,6 +10,25 @@ import (
 	"path/filepath"
 )
 
+// Sound represents a sound instance.
+// See https://www.freesound.org/docs/api/resources_apiv2.html#sound-instance
+type Sound struct {
+	ID          int      `json:"id"`
+	URL         string   `json:"url"`
+	Name        string   `json:"name"`
+	Tags        []string `json:"tags"`
+	Description string   `json:"description"`
+	Geotag      string   `json:"geotag"` // TODO: split into geo.Point
+	Created     string   `json:"created"`
+	License     string   `json:"license"`
+	Type        string   `json:"type"`
+	Channels    int      `json:"channels"`
+	Filesize    int      `json:"filesize"` // Size of the file in bytes.
+	Bitrate     int      `json:"bitrate"`
+	Bitdepth    int      `json:"bitdepth"`
+	Duration    int      `json:"duration"`
+}
+
 // Upload contains optional params for sound upload.
 type Upload struct {
 	Name        string
